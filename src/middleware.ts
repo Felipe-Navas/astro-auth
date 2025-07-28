79195ecc05c62ddc49658143531f0833986df980
Felipe Navas <felipenavas.itec@gmail.com>
2025-07-28 19:44:43 -0300

    Add locals

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,16 @@
 import type { MiddlewareNext } from 'astro'
 import { defineMiddleware } from 'astro:middleware'
+import { firebase } from './firebase/config'
 
 const privateRoutes = ['/protected']
 
 export const onRequest = defineMiddleware((context, next: MiddlewareNext) => {
+  const isLoggedIn = !!firebase.auth.currentUser
+
+  const user = firebase.auth.currentUser
+
+  context.locals.isLoggedIn = isLoggedIn
+  // context.locals.user = user
+
   return next()
 })
